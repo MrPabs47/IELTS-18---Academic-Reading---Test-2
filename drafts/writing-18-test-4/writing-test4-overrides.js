aa4d98a5948cb61bde2ab6a713a581dd4fc5bd6e
MrPabs47 <pablosoad4747@gmail.com>
2026-08-07 02:05:55 +0800

    Use vector graph in IELTS 18 Test 4 preview

--- drafts/writing-18-test-4/writing-test4-overrides.js
+++ drafts/writing-18-test-4/writing-test4-overrides.js
@@ -1,7 +1,7 @@
 (() => {
   const TEST_TITLE = "IELTS 18 Academic Writing Test 4";
   const TEACHER_EMAIL = "pablo.jaramillo@ilsc.com.au";
-  const TASK_IMAGE = "metals-price-change-4k.png";
+  const TASK_IMAGE = "metals-price-change-vector.svg";
 
   document.title = TEST_TITLE;
   document.querySelector("#modeScreenInner h1")?.replaceChildren(TEST_TITLE);
