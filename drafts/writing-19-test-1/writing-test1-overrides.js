6700655fb768742a65295f5b13bd926aafe3088f
MrPabs47 <pablosoad4747@gmail.com>
2026-08-07 13:36:05 +0800

    Use reliable PNG graph asset in IELTS 19 Test 1 preview

--- drafts/writing-19-test-1/writing-test1-overrides.js
+++ drafts/writing-19-test-1/writing-test1-overrides.js
@@ -1,7 +1,7 @@
 (() => {
   const TEST_TITLE = "IELTS 19 Academic Writing Test 1";
   const TEACHER_EMAIL = "pablo.jaramillo@ilsc.com.au";
-  const TASK_IMAGE = "participants-by-activity-enhanced.webp";
+  const TASK_IMAGE = "participants-by-activity-enhanced.png";
 
   document.title = TEST_TITLE;
   document.querySelector("#modeScreenInner h1")?.replaceChildren(TEST_TITLE);
