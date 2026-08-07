236420d7d3c0044480fb2977c46f5639ed55fa11
MrPabs47 <pablosoad4747@gmail.com>
2026-08-07 13:44:35 +0800

    Use reliable PNG harbour asset in IELTS 19 Test 2 preview

--- drafts/writing-19-test-2/writing-test2-overrides.js
+++ drafts/writing-19-test-2/writing-test2-overrides.js
@@ -1,7 +1,7 @@
 (() => {
   const TEST_TITLE = "IELTS 19 Academic Writing Test 2";
   const TEACHER_EMAIL = "pablo.jaramillo@ilsc.com.au";
-  const TASK_IMAGE = "porth-harbour-enhanced.webp";
+  const TASK_IMAGE = "porth-harbour-enhanced.png";
 
   document.title = TEST_TITLE;
   document.querySelector("#modeScreenInner h1")?.replaceChildren(TEST_TITLE);
